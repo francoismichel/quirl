13663a4e5fb581e1c1feeb553c2eb65b7214142c
François Michel <francois.michel@uclouvain.be>
2022-12-16 13:38:32 +0000

    do not close the connection when an unused source symbol is received

--- quiche/src/lib.rs
+++ quiche/src/lib.rs
@@ -382,6 +382,7 @@
 #[macro_use]
 extern crate log;
 
+use networkcoding::DecoderError;
 use networkcoding::EncoderError;
 use networkcoding::SourceSymbolMetadata;
 #[cfg(feature = "qlog")]
@@ -7801,6 +7802,9 @@ impl Connection {
                 }
                 
                 match self.fec_decoder.receive_source_symbol(source_symbol) {
+                    Err(DecoderError::UnusedSourceSymbol) => {
+                        info!("received a source symbol unused by the decoder");
+                    },
                     Err(err) => return Err(Error::from(err)),
                     Ok(decoded_symbols) => {
                         for decoded_symbol in decoded_symbols {
