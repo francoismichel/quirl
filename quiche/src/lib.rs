b0d5102825f6461578ee6e17edab37c4c32ad612
François Michel <francois.michel@uclouvain.be>
2023-01-03 15:03:09 +0000

    ensure to fill the cwin when possible when FEC is enabled to be application-limited

--- quiche/src/lib.rs
+++ quiche/src/lib.rs
@@ -3971,12 +3971,16 @@ impl Connection {
 
         let path = self.paths.get_mut(send_pid)?;
 
+        let left_before_reduction_by_cwnd = left;
         // Limit output packet size by congestion window size.
         left = cmp::min(
             left,
             // Bytes consumed by ACK frames.
             cwnd_available.saturating_sub(left_before_packing_ack_frame - left),
         );
+
+
+        let space_reduction_due_to_cwnd = left_before_reduction_by_cwnd - left;
 
         let mut challenge_data = None;
 
@@ -4415,7 +4419,7 @@ impl Connection {
 
 
         if should_protect_packet {
-            left -= std::cmp::min(32, left)
+            left -= std::cmp::min(32usize.saturating_sub(space_reduction_due_to_cwnd), left);
         }
 
         
