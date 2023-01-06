5a817857e29ea267ca21c3f93b90228d9b401c89
François Michel <francois.michel@uclouvain.be>
2023-01-06 10:26:25 +0000

    FIX: better handling of lost-and-recovered frames

--- quiche/src/lib.rs
+++ quiche/src/lib.rs
@@ -3567,125 +3567,137 @@ impl Connection {
             self.use_path_pkt_num_space(epoch);
         // Process lost frames. There might be several paths having lost frames.
         for (_, p) in self.paths.iter_mut() {
-            for lost in p.recovery.lost[epoch].drain(..) {
-                match lost {
-                    frame::Frame::CryptoHeader { offset, length } => {
-                        self.pkt_num_spaces
-                            .crypto
-                            .get_mut(epoch)
-                            .crypto_stream
-                            .send
-                            .retransmit(offset, length);
+            for lost_frame in p.recovery.lost[epoch].drain(..) {
+                match lost_frame {
+                    recovery::LostFrame::Lost(frame) => {
+                        match frame {
+                            frame::Frame::CryptoHeader { offset, length } => {
+                                self.pkt_num_spaces
+                                    .crypto
+                                    .get_mut(epoch)
+                                    .crypto_stream
+                                    .send
+                                    .retransmit(offset, length);
 
-                        self.stream_retrans_bytes += length as u64;
-                        p.stream_retrans_bytes += length as u64;
+                                self.stream_retrans_bytes += length as u64;
+                                p.stream_retrans_bytes += length as u64;
 
-                        self.retrans_count += 1;
-                        p.retrans_count += 1;
-                    },
+                                self.retrans_count += 1;
+                                p.retrans_count += 1;
+                            },
 
-                    frame::Frame::StreamHeader {
-                        stream_id,
-                        offset,
-                        length,
-                        fin,
-                    } => {
-                        let stream = match self.streams.get_mut(stream_id) {
-                            Some(v) => v,
+                            frame::Frame::StreamHeader {
+                                stream_id,
+                                offset,
+                                length,
+                                fin,
+                            } => {
+                                let stream = match self.streams.get_mut(stream_id) {
+                                    Some(v) => v,
 
-                            None => continue,
-                        };
+                                    None => continue,
+                                };
 
-                        let was_flushable = stream.is_flushable();
+                                let was_flushable = stream.is_flushable();
 
-                        let empty_fin = length == 0 && fin;
+                                let empty_fin = length == 0 && fin;
 
-                        stream.send.retransmit(offset, length);
+                                stream.send.retransmit(offset, length);
 
-                        // If the stream is now flushable push it to the
-                        // flushable queue, but only if it wasn't already
-                        // queued.
-                        //
-                        // Consider the stream flushable also when we are
-                        // sending a zero-length frame that has the fin flag
-                        // set.
-                        if (stream.is_flushable() || empty_fin) && !was_flushable
-                        {
-                            let priority_key = Arc::clone(&stream.priority_key);
-                            self.streams.insert_flushable(&priority_key);
+                                // If the stream is now flushable push it to the
+                                // flushable queue, but only if it wasn't already
+                                // queued.
+                                //
+                                // Consider the stream flushable also when we are
+                                // sending a zero-length frame that has the fin flag
+                                // set.
+                                if (stream.is_flushable() || empty_fin) && !was_flushable
+                                {
+                                    let priority_key = Arc::clone(&stream.priority_key);
+                                    self.streams.insert_flushable(&priority_key);
+                                }
+
+                                self.stream_retrans_bytes += length as u64;
+                                p.stream_retrans_bytes += length as u64;
+
+                                self.retrans_count += 1;
+                                p.retrans_count += 1;
+                            },
+
+                            frame::Frame::ACK { .. } => {
+                                self.pkt_num_spaces
+                                    .spaces
+                                    .get_mut(epoch, 0)
+                                    .map(|pns| {
+                                        pns.ack_elicited = true;
+                                    })
+                                    .ok();
+                            },
+
+                            frame::Frame::ResetStream {
+                                stream_id,
+                                error_code,
+                                final_size,
+                            } =>
+                                if self.streams.get(stream_id).is_some() {
+                                    self.streams
+                                        .insert_reset(stream_id, error_code, final_size);
+                                },
+
+                            // Retransmit HANDSHAKE_DONE only if it hasn't been acked at
+                            // least once already.
+                            frame::Frame::HandshakeDone if !self.handshake_done_acked => {
+                                self.handshake_done_sent = false;
+                            },
+
+                            frame::Frame::MaxStreamData { stream_id, .. } => {
+                                if self.streams.get(stream_id).is_some() {
+                                    self.streams.insert_almost_full(stream_id);
+                                }
+                            },
+
+                            frame::Frame::MaxData { .. } => {
+                                self.almost_full = true;
+                            },
+        
+                            frame::Frame::NewConnectionId { seq_num, .. } => {
+                                self.ids.mark_advertise_new_scid_seq(seq_num, true);
+                            },
+        
+                            frame::Frame::RetireConnectionId { seq_num } => {
+                                self.ids.mark_retire_dcid_seq(seq_num, true);
+                            },
+
+                            frame::Frame::ACKMP {
+                                space_identifier, ..
+                            } => {
+                                self.pkt_num_spaces
+                                    .spaces
+                                    .get_mut(epoch, space_identifier)
+                                    .map(|pns| {
+                                        pns.ack_elicited = true;
+                                    })
+                                    .ok();
+                            },
+
+                            frame::Frame::Repair { .. } => {
+                                if let Some(scheduler) = &mut self.fec_scheduler {
+                                    scheduler.lost_repair_symbol();
+                                }
+                            },
+        
+                            _ => (),
                         }
-
-                        self.stream_retrans_bytes += length as u64;
-                        p.stream_retrans_bytes += length as u64;
-
-                        self.retrans_count += 1;
-                        p.retrans_count += 1;
-                    },
-
-                    frame::Frame::ACK { .. } => {
-                        self.pkt_num_spaces
-                            .spaces
-                            .get_mut(epoch, 0)
-                            .map(|pns| {
-                                pns.ack_elicited = true;
-                            })
-                            .ok();
-                    },
-
-                    frame::Frame::ResetStream {
-                        stream_id,
-                        error_code,
-                        final_size,
-                    } =>
-                        if self.streams.get(stream_id).is_some() {
-                            self.streams
-                                .insert_reset(stream_id, error_code, final_size);
-                        },
-
-                    // Retransmit HANDSHAKE_DONE only if it hasn't been acked at
-                    // least once already.
-                    frame::Frame::HandshakeDone if !self.handshake_done_acked => {
-                        self.handshake_done_sent = false;
-                    },
-
-                    frame::Frame::MaxStreamData { stream_id, .. } => {
-                        if self.streams.get(stream_id).is_some() {
-                            self.streams.insert_almost_full(stream_id);
+                    }
+                    recovery::LostFrame::LostAndRecovered(frame) => {
+                        if let frame::Frame::Repair { .. } = frame {
+                            if let Some(scheduler) = &mut self.fec_scheduler {
+                                scheduler.lost_repair_symbol();
+                            }
                         }
-                    },
-
-                    frame::Frame::MaxData { .. } => {
-                        self.almost_full = true;
-                    },
-
-                    frame::Frame::NewConnectionId { seq_num, .. } => {
-                        self.ids.mark_advertise_new_scid_seq(seq_num, true);
-                    },
-
-                    frame::Frame::RetireConnectionId { seq_num } => {
-                        self.ids.mark_retire_dcid_seq(seq_num, true);
-                    },
-
-                    frame::Frame::ACKMP {
-                        space_identifier, ..
-                    } => {
-                        self.pkt_num_spaces
-                            .spaces
-                            .get_mut(epoch, space_identifier)
-                            .map(|pns| {
-                                pns.ack_elicited = true;
-                            })
-                            .ok();
-                    },
-
-                    frame::Frame::Repair { .. } => {
-                        if let Some(scheduler) = &mut self.fec_scheduler {
-                            scheduler.lost_repair_symbol();
-                        }
-                    },
-
-                    _ => (),
+                    }
                 }
+                
             }
         }
 
--- quiche/src/recovery/mod.rs
+++ quiche/src/recovery/mod.rs
@@ -37,6 +37,7 @@ use crate::Config;
 use crate::Result;
 
 use crate::frame;
+use crate::frame::Frame;
 use crate::minmax;
 use crate::packet;
 use crate::ranges;
@@ -87,6 +88,11 @@ impl SpacedPktNum {
     }
 }
 
+pub enum LostFrame {
+    Lost(Frame),
+    LostAndRecovered(Frame),
+}
+
 pub struct Recovery {
     loss_detection_timer: Option<Instant>,
 
@@ -115,7 +121,7 @@ pub struct Recovery {
 
     sent: [VecDeque<Sent>; packet::Epoch::count()],
 
-    pub lost: [Vec<frame::Frame>; packet::Epoch::count()],
+    pub lost: [Vec<LostFrame>; packet::Epoch::count()],
 
     pub acked: [Vec<frame::Frame>; packet::Epoch::count()],
 
@@ -724,17 +730,16 @@ impl Recovery {
         for unacked in unacked_iter {
             let mut contains_recovered_source_symbol = false;
             for frame in &unacked.frames {
-                match frame {
-                    frame::Frame::SourceSymbolHeader { recovered, .. } => {
-                        if *recovered {
-                            contains_recovered_source_symbol = true;
-                        }
+                if let frame::Frame::SourceSymbolHeader { recovered, .. } = frame {
+                    if *recovered {
+                        contains_recovered_source_symbol = true;
                     }
-                    _ => {
-                        if contains_recovered_source_symbol {
-                            self.lost[epoch].push(frame.clone())
-                        }
-                    }
+                }
+
+                if contains_recovered_source_symbol {
+                    self.lost[epoch].push(LostFrame::LostAndRecovered(frame.clone()))
+                } else {
+                    self.lost[epoch].push(LostFrame::Lost(frame.clone()))
                 }
             }
         }
@@ -983,9 +988,23 @@ impl Recovery {
         ) {
             let mut epoch_lost_bytes = 0;
             let mut largest_lost_pkt = None;
-            for sent in self.sent[e].drain(..) {
+            for mut sent in self.sent[e].drain(..) {
                 if sent.time_acked.is_none() {
-                    self.lost[e].extend_from_slice(&sent.frames);
+                    let mut contains_recovered_source_symbol = false;
+                    for frame in &mut sent.frames.drain(..) {
+                        if let frame::Frame::SourceSymbolHeader { recovered, .. } = frame {
+                            if recovered {
+                                contains_recovered_source_symbol = true;
+                            }
+                        }
+
+                        if contains_recovered_source_symbol {
+                            self.lost[e].push(LostFrame::LostAndRecovered(frame.clone()))
+                        } else {
+                            self.lost[e].push(LostFrame::Lost(frame.clone()))
+                        }
+                    }
+                    
                     if sent.in_flight {
                         epoch_lost_bytes += sent.size;
 
@@ -1052,7 +1071,20 @@ impl Recovery {
             if unacked.time_sent <= lost_send_time ||
                 largest_acked.1 >= unacked.pkt_num.1 + self.pkt_thresh
             {
-                self.lost[epoch].extend(unacked.frames.drain(..));
+                let mut contains_recovered_source_symbol = false;
+                for frame in &mut unacked.frames.drain(..) {
+                    if let frame::Frame::SourceSymbolHeader { recovered, .. } = frame {
+                        if recovered {
+                            contains_recovered_source_symbol = true;
+                        }
+                    }
+
+                    if contains_recovered_source_symbol {
+                        self.lost[epoch].push(LostFrame::LostAndRecovered(frame.clone()))
+                    } else {
+                        self.lost[epoch].push(LostFrame::Lost(frame.clone()))
+                    }
+                }
 
                 unacked.time_lost = Some(now);
 
