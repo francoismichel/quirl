0b2a6434ddeb88744eb692c3f677863ad3f2a794
François Michel <francois.michel@uclouvain.be>
2023-02-27 16:29:06 +0000

    squashed fixes & optimizations

--- quiche/src/fec/background_fec_scheduler.rs
+++ quiche/src/fec/background_fec_scheduler.rs
@@ -1,35 +1,71 @@
 use crate::Connection;
 use crate::path::Path;
 
+const DEFAULT_DELAYING_DURATION: std::time::Duration = std::time::Duration::from_millis(2);
+
 pub struct BackgroundFECScheduler {
+    delaying_duration: std::time::Duration,
     n_repair_in_flight: u64,
+    rs_triggering_time: Option<std::time::Instant>, // can be used to delay the sending of repair symbols (sometimes waiting allows escaping a burst loss event)
+    rs_sent_for_this_round: bool,
 }
 
 impl BackgroundFECScheduler {
     pub fn new() -> BackgroundFECScheduler {
         BackgroundFECScheduler{
-            n_repair_in_flight: 0
+            delaying_duration: DEFAULT_DELAYING_DURATION,
+            n_repair_in_flight: 0,
+            rs_triggering_time: None,
+            rs_sent_for_this_round: false,
         }
     }
 
-    pub fn should_send_repair(&self, conn: &Connection, path: &Path, symbol_size: usize) -> bool {
+    fn reset_rs_delaying(&mut self) {
+        self.rs_triggering_time = None;
+        self.rs_sent_for_this_round = false;
+    }
+
+    pub fn should_send_repair(&mut self, conn: &Connection, path: &Path, symbol_size: usize) -> bool {
+        let now = std::time::Instant::now();
         let dgrams_to_emit = conn.dgram_max_writable_len().is_some();
         let stream_to_emit = conn.streams.has_flushable();
+        if let Ok(val) = std::env::var("DEBUG_QUICHE_FEC_BACKGROUND_DELAYING_DURATION_US") {
+            self.delaying_duration = std::time::Duration::from_micros(val.parse().unwrap_or(DEFAULT_DELAYING_DURATION.as_micros() as u64))
+        }
         // send if no more data to send && we sent less repair than half the cwin
 
-        let cwnd = path.recovery.cwnd();
-        let max_repair_data = if cwnd < 15000 {
-            cwnd*4/5
+        let bif = path.recovery.cwnd() - path.recovery.cwnd_available();
+        let max_repair_data = if bif < symbol_size {
+            0
+        } else if bif < 15000 {
+            bif*4/5
         } else {
-            cwnd/2
+            bif/2
         };
+        
         trace!("fec_scheduler dgrams_to_emit={} stream_to_emit={} n_repair_in_flight={} max_repair_data={}",
                 dgrams_to_emit, stream_to_emit, self.n_repair_in_flight, max_repair_data);
-        !dgrams_to_emit && !stream_to_emit && (self.n_repair_in_flight as usize *symbol_size) < max_repair_data
+        let repair_symbol_required = !dgrams_to_emit && !stream_to_emit && (self.n_repair_in_flight as usize *symbol_size) < max_repair_data;
+        if !repair_symbol_required {
+            self.reset_rs_delaying();
+            false
+        } else {
+            if self.rs_triggering_time.is_none() {
+                // let's start the delaying of the sending of repair symbols
+                self.rs_triggering_time = Some(now);
+                self.rs_sent_for_this_round = false;
+            }
+            trace!("rs_triggering_time = {:?}, waiting remaining = {:?}", self.rs_triggering_time,
+                    self.rs_triggering_time.map(|t| (t + self.delaying_duration).duration_since(now)));
+            let waited_enough = self.rs_triggering_time.is_some() && now >= self.rs_triggering_time.unwrap() + self.delaying_duration;
+    
+            repair_symbol_required && waited_enough
+        }
     }
 
     pub fn sent_repair_symbol(&mut self) {
         self.n_repair_in_flight += 1;
+        self.rs_sent_for_this_round = true;
     }
 
     pub fn acked_repair_symbol(&mut self) {
@@ -38,11 +74,25 @@ impl BackgroundFECScheduler {
     }
     
     pub fn sent_source_symbol(&mut self) {
-
+        // reset the delaying logic, we start a new round as we send new source symbols
+        self.reset_rs_delaying();
     }
 
     pub fn lost_repair_symbol(&mut self) {
         self.acked_repair_symbol()
+    }
+
+    // returns an Instant at which the stack should wake up to sent new repair symbols
+    pub fn timeout(&self) -> Option<std::time::Instant> {
+        if self.rs_sent_for_this_round {
+            None
+        } else {
+            if let Some(triggering_time) = self.rs_triggering_time {
+                Some(triggering_time + self.delaying_duration)
+            } else {
+                None
+            }
+        }
     }
 
 
--- quiche/src/fec/burst_protecting_fec_scheduler.rs
+++ quiche/src/fec/burst_protecting_fec_scheduler.rs
@@ -5,15 +5,18 @@ use std::env;
 #[derive(Debug, Clone, Copy)]
 struct SendingState {
     start_time: std::time::Instant,
-    max_sending_repair_bytes: usize,
+    repair_bytes_to_send: usize,
+    repair_symbols_sent: usize, // number of repair symbols sent during this state
 }
 pub(crate) struct BurstsFECScheduler {
     n_repair_in_flight: u64,
     n_packets_sent_when_nothing_to_send: usize,
-    n_bytes_sent_when_nothing_to_send: usize,
-    n_sent_bytes_when_last_repair: usize,
-    first_source_symbol_in_burst_sent_time: Option<std::time::Instant>,
+    n_sent_stream_bytes_sent_when_nothing_to_send: usize,
+    n_sent_stream_bytes_when_last_repair: usize,
+    earliest_unprotected_source_symbol_sent_time: Option<std::time::Instant>,
+    n_source_symbols_sent_since_last_repair: usize,
     state_sending_repair: Option<SendingState>,
+    delayed_sending: Option<std::time::Instant>,
 }
 
 impl BurstsFECScheduler {
@@ -21,10 +24,12 @@ impl BurstsFECScheduler {
         BurstsFECScheduler{
             n_repair_in_flight: 0,
             n_packets_sent_when_nothing_to_send: 0,
-            n_bytes_sent_when_nothing_to_send: 0,
-            n_sent_bytes_when_last_repair: 0,
-            first_source_symbol_in_burst_sent_time: None,
+            n_sent_stream_bytes_sent_when_nothing_to_send: 0,
+            n_sent_stream_bytes_when_last_repair: 0,
+            earliest_unprotected_source_symbol_sent_time: None,
+            n_source_symbols_sent_since_last_repair: 0,
             state_sending_repair: None,
+            delayed_sending: None,
         }
     }
 
@@ -35,11 +40,18 @@ impl BurstsFECScheduler {
         const DEFAULT_COOLDOWN_US: u64 = 0;
         const DEFAULT_FRAC_DENOMINATOR_TO_PROTECT: usize = 2;
         const DEFAULT_MINIMUM_ROOM_IN_CWIN: usize = 5000;
-        let burst_size: usize = env::var("DEBUG_QUICHE_FEC_BURST_SIZE_BYTES").unwrap_or(DEFAULT_BURST_SIZE.to_string()).parse().unwrap_or(DEFAULT_BURST_SIZE);
+        const DEFAULT_BANDWIDTH_PROBING_BPS: usize = 0;
+        const DEFAULT_SENDING_DELAY_US: u64 = 0;           // the delay value in microseconds
+        let threshold_burst_size: usize = env::var("DEBUG_QUICHE_FEC_BURST_SIZE_BYTES").unwrap_or(DEFAULT_BURST_SIZE.to_string()).parse().unwrap_or(DEFAULT_BURST_SIZE);
+
         let fec_cooldown_us: u64 = env::var("DEBUG_QUICHE_FEC_COOLDOWN_US").unwrap_or(DEFAULT_COOLDOWN_US.to_string()).parse().unwrap_or(DEFAULT_COOLDOWN_US);
         let fec_cooldown = std::time::Duration::from_micros(fec_cooldown_us);
         let fec_frac_denominator_to_protect: usize = env::var("DEBUG_QUICHE_DEFAULT_FRAC_DENOMINATOR_TO_PROTECT").unwrap_or(DEFAULT_FRAC_DENOMINATOR_TO_PROTECT.to_string()).parse().unwrap_or(DEFAULT_FRAC_DENOMINATOR_TO_PROTECT);
         let minimum_room_in_cwin = env::var("DEBUG_QUICHE_MINIMUM_ROOM_IN_CWIN").unwrap_or(DEFAULT_MINIMUM_ROOM_IN_CWIN.to_string()).parse().unwrap_or(DEFAULT_MINIMUM_ROOM_IN_CWIN);
+
+        let sending_delay_us: u64 = env::var("DEBUG_QUICHE_SENDING_DELAY_US").unwrap_or(DEFAULT_SENDING_DELAY_US.to_string()).parse().unwrap_or(DEFAULT_SENDING_DELAY_US);
+        let sending_delay = std::time::Duration::from_micros(sending_delay_us);
+        
         let dgrams_to_emit = conn.dgram_max_writable_len().is_some();
         let stream_to_emit = conn.streams.has_flushable();
         // send if no more data to send && we sent less repair than half the cwin
@@ -49,53 +61,80 @@ impl BurstsFECScheduler {
         let enough_room_in_cwin = path.recovery.cwnd_available() > minimum_room_in_cwin;
         let nothing_to_send = !dgrams_to_emit && !stream_to_emit;
         let current_sent_count = conn.sent_count;
-        let current_sent_bytes = conn.sent_bytes as usize;
-        let sent_enough_protected_data = current_sent_bytes - self.n_bytes_sent_when_nothing_to_send > burst_size;
+        let current_sent_stream_bytes = conn.tx_data as usize;
+        let current_burst_size = current_sent_stream_bytes - self.n_sent_stream_bytes_sent_when_nothing_to_send;
+        let sent_enough_protected_data = current_burst_size > threshold_burst_size;
+        // we should probe using FEC if we are app-limited and the currently sent bitrate is not matching the bandwidth objective
+
         trace!("fec_scheduler dgrams_to_emit={} stream_to_emit={} n_repair_in_flight={} sending_state={:?} sent_count={} old_sent_count={}",
                 dgrams_to_emit, stream_to_emit, self.n_repair_in_flight, self.state_sending_repair, current_sent_count, self.n_packets_sent_when_nothing_to_send);
+
         
-        self.state_sending_repair = match self.state_sending_repair {
-            Some(state) => {
-                if now.duration_since(state.start_time) > path.recovery.rtt() {
-                    None
-                } else {
-                    Some(state)
-                }
-            }
-            None => {
-                if nothing_to_send && sent_enough_protected_data && enough_room_in_cwin
-                    && (self.first_source_symbol_in_burst_sent_time.is_none() || now > self.first_source_symbol_in_burst_sent_time.unwrap() + fec_cooldown) {
-                    // a burst of packets has occurred, so send repair symbols
-                    let bytes_to_protect = std::cmp::min(bif, current_sent_bytes - self.n_sent_bytes_when_last_repair);
-                    let max_repair_data = if bytes_to_protect < 15000 {
-                        bytes_to_protect*3/5
+        self.state_sending_repair = if nothing_to_send && sent_enough_protected_data
+                                       && (self.earliest_unprotected_source_symbol_sent_time.is_none() 
+                                           || now > self.earliest_unprotected_source_symbol_sent_time.unwrap() + fec_cooldown) {
+            // a burst of packets has occurred, so send repair symbols
+            let bytes_to_protect = std::cmp::min(bif, self.n_source_symbols_sent_since_last_repair*symbol_size);
+            let max_repair_data = if bytes_to_protect < 15000 {
+                bytes_to_protect*3/5
+            } else {
+                bytes_to_protect/fec_frac_denominator_to_protect
+            };
+
+            // if there exists a previous state, reset the delaying deadline if it is an old state, otherwise keep the
+            // existing deadline if it exists 
+            self.delayed_sending = match self.state_sending_repair {
+                None => {
+                    Some(now + sending_delay)
+                },
+                Some(state) => {
+                    let state_expired = now.duration_since(state.start_time) >= path.recovery.rtt();
+                    if self.delayed_sending.is_none() || state_expired {
+                        Some(now + sending_delay)
                     } else {
-                        bytes_to_protect/fec_frac_denominator_to_protect
-                    };
-                    Some(SendingState{start_time: now, max_sending_repair_bytes: max_repair_data})
-                } else {
-                    None
+                        // if there exists a state and the delay is not none, keep the same delay
+                        self.delayed_sending
+                    }
                 }
+            };
+
+            Some(SendingState{start_time: now, repair_bytes_to_send: max_repair_data, repair_symbols_sent: 0})
+        } else {
+            // the state expires after 1 RTT
+            if let Some(state) = self.state_sending_repair {
+                if now.duration_since(state.start_time) >= path.recovery.rtt() {
+                    None
+                } else {
+                    self.state_sending_repair
+                }
+            } else {
+                None
             }
         };
-
         if nothing_to_send {
             self.n_packets_sent_when_nothing_to_send = conn.sent_count;
-            self.n_bytes_sent_when_nothing_to_send = conn.sent_bytes as usize;
+            self.n_sent_stream_bytes_sent_when_nothing_to_send = conn.tx_data as usize;
         }
-        let should_send = match self.state_sending_repair {
-            Some(state) => (self.n_repair_in_flight as usize * symbol_size) < state.max_sending_repair_bytes,
+        let should_send = (match self.state_sending_repair {
+            Some(state) => {
+                (state.repair_symbols_sent * symbol_size) < state.repair_bytes_to_send && now >= self.delayed_sending.unwrap_or(now)
+            }
             None => false,
-        };
+        });
         if should_send {
-            self.n_sent_bytes_when_last_repair = current_sent_bytes;
+            self.n_sent_stream_bytes_when_last_repair = current_sent_stream_bytes;
         }
         should_send
     }
 
     pub fn sent_repair_symbol(&mut self) {
         self.n_repair_in_flight += 1;
-        self.first_source_symbol_in_burst_sent_time = None;
+        self.earliest_unprotected_source_symbol_sent_time = None;
+        self.n_source_symbols_sent_since_last_repair = 0;
+        self.delayed_sending = None;
+        if let Some(state) = &mut self.state_sending_repair {
+            state.repair_symbols_sent += 1;
+        }
     }
 
     pub fn acked_repair_symbol(&mut self) {
@@ -103,14 +142,19 @@ impl BurstsFECScheduler {
     }
 
     pub fn sent_source_symbol(&mut self) {
-        if let None = self.first_source_symbol_in_burst_sent_time {
-            self.first_source_symbol_in_burst_sent_time = Some(std::time::Instant::now());
+        if let None = self.earliest_unprotected_source_symbol_sent_time {
+            self.earliest_unprotected_source_symbol_sent_time = Some(std::time::Instant::now());
         }
+        self.n_source_symbols_sent_since_last_repair += 1;
     }
 
     pub fn lost_repair_symbol(&mut self) {
         self.acked_repair_symbol()
     }
 
+    // returns an Instant at which the stack should wake up to sent new repair symbols
+    pub fn timeout(&self) -> Option<std::time::Instant> {
+        None
+    }
 
 }
