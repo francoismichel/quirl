8741a74cb4574597c075780cb0107d6e9acbcafb
François Michel <francois.michel@uclouvain.be>
2023-05-16 13:09:10 +0200

    FEC add notion of both send and receive window

--- quiche/src/lib.rs
+++ quiche/src/lib.rs
@@ -493,8 +493,11 @@ const MAX_PROBING_TIMEOUTS: usize = 3;
 // The default initial congestion window size in terms of packet count.
 const DEFAULT_INITIAL_CONGESTION_WINDOW_PACKETS: usize = 10;
 
-// the default FEC window size in terms of source symbols
-const DEFAULT_FEC_WINDOW_SIZE: usize = 500;
+// Window size for the FEC sender, in number of source symbols
+const DEFAULT_FEC_SEND_WINDOW_SIZE: usize = 5000;
+
+// Window size for the FEC receiver, in number of source symbols
+const DEFAULT_FEC_RECEIVE_WINDOW_SIZE: usize = 5000;
 /// A specialized [`Result`] type for quiche operations.
 ///
 /// This type is used throughout quiche's public API for any operation that
@@ -789,7 +792,8 @@ pub struct Config {
     fec_scheduler_algorithm: FECSchedulerAlgorithm,
     emit_fec: bool,
     receive_fec: bool,
-    fec_window_size: usize,
+    fec_receive_window_size: usize,
+    fec_send_window_size: usize,
 
     real_time: bool,
 }
@@ -859,7 +863,10 @@ impl Config {
             fec_scheduler_algorithm: std::env::var("QUICHE_FEC_OVERRIDE_FEC_SCHEDULER").unwrap_or_default().parse().unwrap_or(FECSchedulerAlgorithm::NoRedundancy),
             emit_fec:  std::env::var("QUICHE_FEC_OVERRIDE_EMIT_FEC").unwrap_or_default().parse().unwrap_or(0) != 0,
             receive_fec: std::env::var("QUICHE_FEC_OVERRIDE_RECEIVE_FEC").unwrap_or_default().parse().unwrap_or(0) != 0,
-            fec_window_size: DEFAULT_FEC_WINDOW_SIZE,
+
+            fec_receive_window_size: DEFAULT_FEC_RECEIVE_WINDOW_SIZE,
+            fec_send_window_size: DEFAULT_FEC_SEND_WINDOW_SIZE,
+
             real_time: false,
         })
     }
@@ -1325,9 +1332,16 @@ impl Config {
 
     /// Sets the FEC decoding window size.
     ///
-    /// The default value is `DEFAULT_FEC_WINDOW_SIZE`.
-    pub fn set_fec_window_size(&mut self, size: usize) {
-        self.fec_window_size = size;
+    /// The default value is `DEFAULT_FEC_RECEIVE_WINDOW_SIZE`.
+    pub fn set_fec_receive_window_size(&mut self, size: usize) {
+        self.fec_receive_window_size = size;
+    }
+    
+    /// Sets the FEC encoding window size.
+    ///
+    /// The default value is `DEFAULT_FEC_SEND_WINDOW_SIZE`.
+    pub fn set_fec_send_window_size(&mut self, size: usize) {
+        self.fec_send_window_size = size;
     }
 
     /// decides whether FEC should be sent to protect data
@@ -1549,7 +1563,8 @@ pub struct Connection {
     emit_fec: bool,
     receive_fec: bool,
     fec_scheduler: Option<fec::fec_scheduler::FECScheduler>,
-    fec_window_size: usize,
+    fec_receive_window_size: usize,
+    _fec_send_window_size: usize,
     recovered_symbols_need_ack: ranges::RangeSet,
     // for stats purpose, keep the metadata of the recovered source symbols
     recovered_symbols_md_history: std::collections::HashMap<u64, RecoveredSymbol>,
@@ -2006,15 +2021,17 @@ impl Connection {
 
             dcid_seq_to_abandon: VecDeque::new(),
             
-            fec_encoder: networkcoding::Encoder::VLC(VLCEncoder::new(config.max_send_udp_payload_size - max_pkt_header_size - max_crypto_overhead - 21, 5000)),
-            fec_decoder: networkcoding::Decoder::VLC(VLCDecoder::new(config.max_send_udp_payload_size - max_pkt_header_size - max_crypto_overhead - 21, 5000)),
+            fec_encoder: networkcoding::Encoder::VLC(VLCEncoder::new(config.max_send_udp_payload_size - max_pkt_header_size - max_crypto_overhead - 21, config.fec_send_window_size)),
+            fec_decoder: networkcoding::Decoder::VLC(VLCDecoder::new(config.max_send_udp_payload_size - max_pkt_header_size - max_crypto_overhead - 21, config.fec_receive_window_size)),
+
             fec_scheduler: Some(fec::fec_scheduler::new_fec_scheduler(config.fec_scheduler_algorithm)),
             latest_metadata_of_symbol_with_fec_protected_frames: None,
             recovered_symbols_md_history: std::collections::HashMap::new(),
 
             emit_fec: config.emit_fec,
             receive_fec: config.receive_fec,
-            fec_window_size: config.fec_window_size,
+            fec_receive_window_size: config.fec_receive_window_size,
+            _fec_send_window_size: config.fec_send_window_size,
             recovered_symbols_need_ack: ranges::RangeSet::new(crate::MAX_ACK_RANGES),
         };
 
@@ -3761,6 +3778,8 @@ impl Connection {
             .pkt_num_spaces
             .spaces
             .get_mut_or_create(epoch, space_id);
+
+        let space_reduction_due_to_cwnd = b.cap() - left;
 
         let pn = pkt_space.next_pkt_num;
         let pn_len = packet::pkt_num_len(pn)?;
@@ -7926,8 +7945,8 @@ impl Connection {
             frame::Frame::SourceSymbol { source_symbol } => {
                 if self.receive_fec {
                     let id = source_symbol_metadata_to_u64(source_symbol.metadata());
-                    if self.fec_window_size as u64 <= id {
-                        self.fec_decoder.remove_up_to(source_symbol_metadata_from_u64(id - self.fec_window_size as u64));
+                    if self.fec_receive_window_size as u64 <= id {
+                        self.fec_decoder.remove_up_to(source_symbol_metadata_from_u64(id - self.fec_receive_window_size as u64));
                     }
 
                     match self.fec_decoder.receive_source_symbol(source_symbol) {
